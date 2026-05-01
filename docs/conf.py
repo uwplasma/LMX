1513c51443d255ea9f11fc5196b1395362e0dd96
Rogerio Jorge <rogerio.jorge@wisc.edu>
2026-05-01 08:46:23 -0500

    Speed up bounded CI release gates

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ from datetime import datetime
 project = "LMX"
 author = "LMX contributors"
 copyright = f"{datetime.now().year}, LMX contributors"
-release = "1.1.1"
+release = "1.1.2"
 
 extensions = [
     "myst_parser",
